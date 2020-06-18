b0048ea46be66a30e7a9b8766042f1c8569a5afd
Tiziano Santoro <tiziano88@gmail.com>
2020-06-18 23:52:16 +0100

    Filtering

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 class UserEvent {
 }
+class Issue {
+}
+class PullRequest {
+}
 function map_append(map, key, value) {
     const current = map.get(key);
     if (current == undefined) {
@@ -20,49 +24,80 @@ function map_append(map, key, value) {
     }
 }
 function main() {
-    var _a, _b, _c, _d, _e, _f;
     return __awaiter(this, void 0, void 0, function* () {
         let username = document.getElementById("username")
             .value;
-        let res = yield fetch(`https://api.github.com/users/${username}/events?per_page=1000`);
-        let events = yield res.json();
-        let events_by_type = events.reduce((map, o) => {
-            map_append(map, o.type, o);
-            return map;
-        }, new Map());
-        console.log(events_by_type);
+        let start_date = Date.parse(document.getElementById("start_date").value);
+        let end_date = Date.parse(document.getElementById("end_date").value);
+        let all_events = new Array();
+        let res = [1, 2, 3].map((page) => fetch(`https://api.github.com/users/${username}/events?per_page=1000&page=${page}`));
+        for (let r of res) {
+            let r1 = yield r;
+            all_events.push(...(yield r1.json()));
+        }
+        console.log(all_events);
+        let events = all_events.filter((e) => {
+            return (Date.parse(e.created_at) >= start_date &&
+                Date.parse(e.created_at) < end_date);
+        });
+        console.log(events);
+        const issues = new Map();
+        const pull_requests = new Map();
         let output = "";
         output += "Opened issues:\n\n";
-        output += (_a = events_by_type
-            .get("IssuesEvent")) === null || _a === void 0 ? void 0 : _a.filter((v) => v.payload.action == "opened").map((v) => `- [${v.payload.issue.title}](${v.payload.issue.url})`).join("\n");
+        output += events
+            .filter((v) => v.type == "IssuesEvent")
+            .filter((v) => v.payload.action == "opened")
+            .map((v) => {
+            var _a;
+            return `- [${v.payload.issue.title}](${v.payload.issue.html_url}) (assignee: ${((_a = v.payload.issue.assignee) === null || _a === void 0 ? void 0 : _a.login) || "n/a"})`;
+        })
+            .join("\n");
         output += "\n\n";
         output += "Reopened issues:\n\n";
-        output += (_b = events_by_type
-            .get("IssuesEvent")) === null || _b === void 0 ? void 0 : _b.filter((v) => v.payload.action == "reopened").map((v) => `- [${v.payload.issue.title}](${v.payload.issue.url})`).join("\n");
+        output += events
+            .filter((v) => v.type == "IssuesEvent")
+            .filter((v) => v.payload.action == "reopened")
+            .map((v) => {
+            var _a;
+            return `- [${v.payload.issue.title}](${v.payload.issue.html_url}) (assignee: ${((_a = v.payload.issue.assignee) === null || _a === void 0 ? void 0 : _a.login) || "n/a"})`;
+        })
+            .join("\n");
         output += "\n\n";
         output += "Commented on issues:\n\n";
-        output += Array.from(((_c = events_by_type
-            .get("IssueCommentEvent")) === null || _c === void 0 ? void 0 : _c.filter((v) => v.payload.action == "created").reduce((map, e) => {
-            map_append(map, e.payload.issue.url, e);
+        output += Array.from(events
+            .filter((v) => v.type == "IssueCommentEvent")
+            .filter((v) => v.payload.action == "created")
+            .reduce((map, e) => {
+            map_append(map, e.payload.issue.html_url, e);
             return map;
-        }, new Map()).entries()) || [])
+        }, new Map())
+            .entries() || [])
             .map(([k, v]) => `- [${v[0].payload.issue.title}](${k}) (author: ${v[0].payload.issue.user.login}, comments: ${v.length})`)
             .join("\n");
         output += "\n\n";
         output += "Opened pull requests:\n\n";
-        output += (_d = events_by_type
-            .get("PullRequestEvent")) === null || _d === void 0 ? void 0 : _d.filter((v) => v.payload.action == "opened").map((v) => `- [${v.payload.pull_request.title}](${v.payload.pull_request.url})`).join("\n");
+        output += events
+            .filter((v) => v.type == "PullRequestEvent")
+            .filter((v) => v.payload.action == "opened")
+            .map((v) => `- [${v.payload.pull_request.title}](${v.payload.pull_request.html_url})`)
+            .join("\n");
         output += "\n\n";
         output += "Closed pull requests:\n\n";
-        output += (_e = events_by_type
-            .get("PullRequestEvent")) === null || _e === void 0 ? void 0 : _e.filter((v) => v.payload.action == "closed").map((v) => `- [${v.payload.pull_request.title}](${v.payload.pull_request.url})`).join("\n");
+        output += events
+            .filter((v) => v.type == "PullRequestEvent")
+            .filter((v) => v.payload.action == "closed")
+            .map((v) => `- [${v.payload.pull_request.title}](${v.payload.pull_request.html_url})`)
+            .join("\n");
         output += "\n\n";
         output += "Commented on pull requests:\n\n";
-        output += Array.from(((_f = events_by_type
-            .get("PullRequestReviewCommentEvent")) === null || _f === void 0 ? void 0 : _f.reduce((map, e) => {
-            map_append(map, e.payload.pull_request.url, e);
+        output += Array.from(events
+            .filter((v) => v.type == "PullRequestReviewCommentEvent")
+            .reduce((map, e) => {
+            map_append(map, e.payload.pull_request.html_url, e);
             return map;
-        }, new Map()).entries()) || [])
+        }, new Map())
+            .entries() || [])
             .map(([k, v]) => `- [${v[0].payload.pull_request.title}](${k}) (author: ${v[0].payload.pull_request.user.login}, comments: ${v.length})`)
             .join("\n");
         output += "\n\n";
--- main.ts
+++ main.ts
@@ -3,6 +3,16 @@
 class UserEvent {
   type!: string;
   payload!: any;
+  created_at!: string;
+}
+
+class Issue {}
+
+class PullRequest {
+  html_url!: string;
+  state!: string;
+  merged!: boolean;
+  title!: string;
 }
 
 function map_append<K, V>(map: Map<K, V[]>, key: K, value: V) {
@@ -17,42 +27,75 @@ function map_append<K, V>(map: Map<K, V[]>, key: K, value: V) {
 async function main() {
   let username = (document.getElementById("username") as HTMLInputElement)
     .value;
-  let res = await fetch(
-    `https://api.github.com/users/${username}/events?per_page=1000`
-  );
-  let events: UserEvent[] = await res.json();
 
-  let events_by_type = events.reduce((map, o: UserEvent) => {
-    map_append(map, o.type, o);
-    return map;
-  }, new Map<string, UserEvent[]>());
-  console.log(events_by_type);
+  let start_date = Date.parse(
+    (document.getElementById("start_date") as HTMLInputElement).value
+  );
+  let end_date = Date.parse(
+    (document.getElementById("end_date") as HTMLInputElement).value
+  );
+
+  let all_events = new Array<UserEvent>();
+
+  let res = [1, 2, 3].map((page) =>
+    fetch(
+      `https://api.github.com/users/${username}/events?per_page=1000&page=${page}`
+    )
+  );
+
+  for (let r of res) {
+    let r1 = await r;
+    all_events.push(...(await r1.json()));
+  }
+
+  console.log(all_events);
+
+  let events = all_events.filter((e) => {
+    return (
+      Date.parse(e.created_at) >= start_date &&
+      Date.parse(e.created_at) < end_date
+    );
+  });
+  console.log(events);
+
+  const issues = new Map<string, Issue>();
+  const pull_requests = new Map<string, PullRequest>();
 
   let output = "";
 
   output += "Opened issues:\n\n";
-  output += events_by_type
-    .get("IssuesEvent")
-    ?.filter((v) => v.payload.action == "opened")
-    .map((v) => `- [${v.payload.issue.title}](${v.payload.issue.url})`)
+  output += events
+    .filter((v) => v.type == "IssuesEvent")
+    .filter((v) => v.payload.action == "opened")
+    .map(
+      (v) =>
+        `- [${v.payload.issue.title}](${v.payload.issue.html_url}) (assignee: ${
+          v.payload.issue.assignee?.login || "n/a"
+        })`
+    )
     .join("\n");
   output += "\n\n";
 
   output += "Reopened issues:\n\n";
-  output += events_by_type
-    .get("IssuesEvent")
-    ?.filter((v) => v.payload.action == "reopened")
-    .map((v) => `- [${v.payload.issue.title}](${v.payload.issue.url})`)
+  output += events
+    .filter((v) => v.type == "IssuesEvent")
+    .filter((v) => v.payload.action == "reopened")
+    .map(
+      (v) =>
+        `- [${v.payload.issue.title}](${v.payload.issue.html_url}) (assignee: ${
+          v.payload.issue.assignee?.login || "n/a"
+        })`
+    )
     .join("\n");
   output += "\n\n";
 
   output += "Commented on issues:\n\n";
   output += Array.from(
-    events_by_type
-      .get("IssueCommentEvent")
-      ?.filter((v) => v.payload.action == "created")
+    events
+      .filter((v) => v.type == "IssueCommentEvent")
+      .filter((v) => v.payload.action == "created")
       .reduce((map, e) => {
-        map_append(map, e.payload.issue.url, e);
+        map_append(map, e.payload.issue.html_url, e);
         return map;
       }, new Map<string, UserEvent[]>())
       .entries() || []
@@ -65,33 +108,33 @@ async function main() {
   output += "\n\n";
 
   output += "Opened pull requests:\n\n";
-  output += events_by_type
-    .get("PullRequestEvent")
-    ?.filter((v) => v.payload.action == "opened")
+  output += events
+    .filter((v) => v.type == "PullRequestEvent")
+    .filter((v) => v.payload.action == "opened")
     .map(
       (v) =>
-        `- [${v.payload.pull_request.title}](${v.payload.pull_request.url})`
+        `- [${v.payload.pull_request.title}](${v.payload.pull_request.html_url})`
     )
     .join("\n");
   output += "\n\n";
 
   output += "Closed pull requests:\n\n";
-  output += events_by_type
-    .get("PullRequestEvent")
-    ?.filter((v) => v.payload.action == "closed")
+  output += events
+    .filter((v) => v.type == "PullRequestEvent")
+    .filter((v) => v.payload.action == "closed")
     .map(
       (v) =>
-        `- [${v.payload.pull_request.title}](${v.payload.pull_request.url})`
+        `- [${v.payload.pull_request.title}](${v.payload.pull_request.html_url})`
     )
     .join("\n");
   output += "\n\n";
 
   output += "Commented on pull requests:\n\n";
   output += Array.from(
-    events_by_type
-      .get("PullRequestReviewCommentEvent")
-      ?.reduce((map, e) => {
-        map_append(map, e.payload.pull_request.url, e);
+    events
+      .filter((v) => v.type == "PullRequestReviewCommentEvent")
+      .reduce((map, e) => {
+        map_append(map, e.payload.pull_request.html_url, e);
         return map;
       }, new Map<string, UserEvent[]>())
       .entries() || []
